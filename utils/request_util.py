29b7619115f3646958c21e01766f65cb39525308
Jimmy Maldonado <jimmy.maldonado@abila.com>
2019-02-22 19:01:01 -0400

    updated request util with json generator

--- utils/request_util.py
+++ utils/request_util.py
@@ -1,21 +1,21 @@
 import requests 
-
+from JSONGenerator import get_json
 
 def get_request(url, params_as_tuple):
-	payload = get_payload(params_as_tuple)
-	requests.get(url, params=payload)
+	payload = get_json(params_as_tuple)
+	return requests.get(url, params=payload)
 
 
 def post_request(url, params_as_tuple):
-	payload = get_payload(params_as_tuple)
-	requests.get(url, params=payload)
+	payload = get_json(params_as_tuple)
+	return requests.post(url, params=payload)
 
 
 def put_request(url, params_as_tuple):
-	payload = get_payload(params_as_tuple)
-	requests.get(url, params=payload)
+	payload = get_json(params_as_tuple)
+	return requests.put(url, params=payload)
 
 
 def delete_request(url, params_as_tuple):
-	payload = get_payload(params_as_tuple)
-	requests.get(url, params=payload)
+	payload = get_json(params_as_tuple)
+	return requests.delete(url, params=payload)
