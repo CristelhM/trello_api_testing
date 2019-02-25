ccc2dd0155b13b8fe74806d3a08ee1755d42fab0
Efrain <aragon0302@gmail.com>
2019-02-25 17:43:47 -0400

    Anadiendo otro scenario y se corrigio las observaciones

--- utils/ids.py
+++ utils/ids.py
@@ -1,4 +1,4 @@
-idBoard = '5c743ccd18610d2e33aca11e'
-idList = '5c743ccee55e0e50d7336619'
-idCard = '5c743ccf18a72c7cc8eca77b'
-idChecklists = '5c743cd1a75a2e7c5985fae5'
+idBoard = '5c7460ac664a05767cadc34e'
+idList = '5c7460ada585fe55f1d89256'
+idCard = '5c7460ae0dc16a43019e9c90'
+idChecklists = '5c7460b09776d308a7512995'
