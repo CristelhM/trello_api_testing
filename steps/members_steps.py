1f6d9e0b25b7d20c7a85fe8cec6a45aaed32371a
Nadim Diaz <nadim1232@gmail.com>
2019-02-24 20:49:54 -0400

    using old board creation to avoid merge conflicts

--- steps/members_steps.py
+++ steps/members_steps.py
@@ -3,6 +3,19 @@ import json
 from compare import expect
 import collections
 
+@given(u'I send a POST request to "{endpoint}"')
+def step_impl(context, endpoint):
+
+    query_params = ({"key": context.key}, {"token": context.token})
+    for row in context.table:
+        query_params += ({row["key"]: row["value"]}, )
+
+    context.response = request_util.post_request(context.base_url + endpoint, query_params)
+
+    data = context.response.json()
+    idRecord = data['id']
+    context.boardId = idRecord
+
 @given(u'I add a new member to "{endpoint}"')
 def step_impl(context,endpoint):
     query_params = ({"key": context.key}, {"token": context.token})
@@ -30,10 +43,8 @@ def step_impl(context,endpoint):
     query_params = ({"key": context.key}, {"token": context.token})
 
     endpoint = endpoint.replace('{id}',context.boardId)
-    print('endpoint::::', endpoint)
     context.getResponse = request_util.get_request(context.base_url + endpoint, query_params)
     context.response = context.getResponse
-    print('get::context.response::::', context.response)
 
 @when(u'I delete a member using "{endpoint}"')
 def step_impl(context,endpoint):
