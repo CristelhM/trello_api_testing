cb082050eccc86198ceb63be778aae82a7af1f24
Efrain <aragon0302@gmail.com>
2019-02-25 15:13:56 -0400

    Add checkclist

---
+++ steps/checklists_steps.py
@@ -0,0 +1,104 @@
+from compare import expect
+from utils import request_util
+from utils import ids
+
+@given(u'que el nombre de Board es: {name}')
+def step_impl(context, name):
+    context.nameBoard = name
+
+@given(u'y el nombre de la List es: {name}')
+def step_impl(context, name):
+    context.nameList = name
+
+@given(u'y el nombre de la Card es: {name}')
+def step_impl(context, name):
+    context.nameCard = name
+
+@given(u'y el nombre del Checklist es: {name}')
+def step_impl(context, name):
+    context.nameChecklist = name
+
+@when(u'se envia un POST')
+def step_impl(context):
+
+    url_board = context.base_url+"/boards"
+    query_board = context.query_params + ({"name":context.nameBoard},{"prefs_background":"lime"},{"defaultLists":"false"})
+    content_board = (request_util.post_request(url_board,query_board)).json()
+    idBoard = content_board["id"]
+    f = open("utils\ids.py","w")
+    f.write("idBoard = '"+str(idBoard)+"'")
+
+    url_list = context.base_url+"/lists"
+    query_list = context.query_params + ({"name":context.nameList},{"idBoard":idBoard})
+    content_list = (request_util.post_request(url_list,query_list)).json()
+    idList = content_list["id"]
+    f.write("\nidList = '"+str(idList)+"'")
+
+    url_card = context.base_url+"/cards"
+    query_card = context.query_params + ({"idList":idList},{"name":context.nameCard})
+    content_card = (request_util.post_request(url_card,query_card)).json()
+    idCard = content_card["id"]
+    f.write("\nidCard = '" + str(idCard) + "'")
+
+    url_checklist = context.base_url + "/checklists"
+    query_checklists = context.query_params + ({"idCard": idCard}, {"name": context.nameChecklist})
+    context.response = request_util.post_request(url_checklist, query_checklists)
+    content_checklists = context.response.json()
+    idChecklists = content_checklists["id"]
+    f.write("\nidChecklists = '" + str(idChecklists) + "'")
+
+    f.closed
+
+@then(u'El status codigo debe ser {numero:d}')
+def step_impl(context,numero):
+    expect(context.response.status_code).to_equal(numero)
+
+# -----------------------------------------------------------------------
+@when(u'se envia un metodo GET con el siguiente idChecklist')
+def step_impl(context):
+    for row in context.table:
+      id=  row["idChecklist"]
+    query = context.query_params
+    url = context.base_url+"/checklists/"+id
+    context.checklist = (request_util.get_request(url,query)).status_code
+
+
+@then(u'el status code debe ser {numero:d}')
+def step_impl(context,numero):
+    expect(int(context.checklist)).to_equal(numero)
+
+# -----------------------------------------------------------------------
+
+@when(u'se envia un GET con un id invalido: {id}')
+def step_impl(context,id):
+    url = context.base_url+"/checklists/"+id
+    context.statuscode = (request_util.get_request(url,context.query_params)).status_code
+
+@then(u'el resultado de status code debe ser {numero:d}')
+def step_impl(context,numero):
+    expect(int(context.statuscode)).to_equal(numero)
+# --------------------------------------------------------------------------
+
+@given(u'que el nombre del check item es: {name}')
+def step_impl(context, name):
+    context.nameitem = name
+
+@given(u'y la posicion del check item en el checklist es: {pos:w}')
+def step_impl(context,pos):
+    context.positem = pos
+
+@given(u'y determinar si el check item ya esta tiqueado cuando se crea {checked:w}')
+def step_impl(context,checked):
+    context.checkeditem = checked
+
+@when(u'se envia un POST al checklist creado en el primer escenario')
+def step_impl(context):
+    url= context.base_url+"/checklists/"+ids.idChecklists+"/checkItems"
+    query_checkitems = context.query_params + ({"name": context.nameitem}, {"pos": context.positem},{"checked":context.checkeditem})
+    context.responseitem = (request_util.post_request(url,query_checkitems)).status_code
+
+@then(u'el resultado de status code de un check item debe ser {numero:d}')
+def step_impl(context, numero):
+    expect(int(context.responseitem)).to_equal(numero)
+# -----------------------------------------------------------------------------------
+
---
+++ utils/ids.py
@@ -0,0 +1,4 @@
+idBoard = '5c743ccd18610d2e33aca11e'
+idList = '5c743ccee55e0e50d7336619'
+idCard = '5c743ccf18a72c7cc8eca77b'
+idChecklists = '5c743cd1a75a2e7c5985fae5'
